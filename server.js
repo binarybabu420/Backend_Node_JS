4b18c48b33bafff4887d08c8658a630616a8dadc
binarybabu420 <sushant09450@gmail.com>
2023-06-14 11:32:07 +0530

    all the code with express js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const http=require('http');
 const routes=require('./routes');
-
-const server=http.createServer(routes.handler);
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middle ware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send({key:'value'});
+})
+const server=http.createServer(app);
 server.listen(5501);
