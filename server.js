c947c993a95f97d33c6878c3c27e76b6ad1bb711
binarybabu420 <sushant09450@gmail.com>
2023-06-19 20:55:40 +0530

    ExpressJS add-product 1

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const http=require('http');
 const routes=require('./routes');
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-    console.log('In the middle ware');
-    next();
+const bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/about-product',(req,res,next)=>{
+    // res.write('<html><body><h1>HELOO</h1></body></html>')
+    // res.send();
+    res.send(`<html><body><form action="/product" method="POST"><label for='product'>Product</label><input type='text' id='product' name='product'><label for='email'>Size</label><input type='text' id='email' name='email'><button>submit</button type submit></form></body></html>`)
+//
+    //next();
 });
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send({key:'value'});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    //console.log(req.body);
+    res.send("<h1>HELLO</h1>");
 })
 const server=http.createServer(app);
 server.listen(5501);
